a27a6e40017a67c2430e8f3af300df32c992c804
LuccasPoontes <luccaspontes14@gmail.com>
2024-06-23 20:23:42 -0300

    Realizado a criação de Scanner conforme solicitado, exibição de mensagem para o usuario, obter o retorno dos valores digitado pelo usuario no tema final. Por fim exibir a mensagem de conta criada!

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        // Exibir as mensagens para o nosso usuário
+        System.out.println("Bem-vindo ao Banco Callyn.");
+        System.out.print("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();  // Consumir a nova linha deixada pelo nextInt
+        
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+        
+        // Exibir a mensagem conta criada
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("Agência: " + agencia);
+        System.out.println("Número da Conta: " + numeroConta);
+        System.out.println("Cliente: " + nomeCliente);
+        System.out.println("Saldo: R$ " + saldo);
+        
+        // Fechar o scanner
+        scanner.close();
     }
 }
